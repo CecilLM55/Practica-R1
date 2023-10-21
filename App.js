c26e6233d7359f82fe4d429e46b22a1ea895e2a2
CecilLM55 <clopez16@ucol.mx>
2023-10-21 15:28:03 -0600

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, Button, Switch, TextInput, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchValue: false,
+      textValue: "",
+    };
+  }
+
+  simpleAlert = () => {
+    Alert.alert(
+      'Texto', `${this.state.textValue}`, 
+      [
+        {
+          text: 'OK',
+          onPress: () => {
+            console.log('Yes Pressed');
+          }
+        }
+      ]
+    );
+  }
+
+  onChange = (value) => {
+    console.warn(`El switch cambiará a: ${value}`);
+    this.setState({switchValue: value});
+  }
+
+  onChangeTextInput = (value) => {
+    this.setState({textValue: value})
+    //console.warn(`Se ha escrito esto: ${value}`);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={this.simpleAlert}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.onChangeTextInput(text)}
+          placeholder="Escribe algo..."
+          value = {this.state.textValue}
+        />
+
+        <Switch
+          onValueChange={() => this.onChange(!this.state.switchValue)}
+          value={this.state.switchValue}
+        />
+
+        <Text>¡Hola, esta es la Práctica R1!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +71,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
